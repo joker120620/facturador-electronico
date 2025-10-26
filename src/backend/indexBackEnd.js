4da26c89ead66e73bad549b4e41bd064389c871d
Juan David <davidtoloza1234@hotmail.com>
2025-10-26 16:19:41 -0500

    con cors 3

--- src/backend/indexBackEnd.js
+++ src/backend/indexBackEnd.js
@@ -730,6 +730,6 @@ app.get("/token", async (req, res) => {
 });
 // Puerto
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
