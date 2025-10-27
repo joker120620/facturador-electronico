c0249e27def6b081d660419a9be8b057b7a51976
Juan David <davidtoloza1234@hotmail.com>
2025-10-26 19:17:36 -0500

    cambiar url cors 3

--- src/backend/indexBackEnd.js
+++ src/backend/indexBackEnd.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 // También puedes mantener cors() por compatibilidad
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',
+  origin: 'https://facturabot.dev',
   credentials: true,
 }));
 // Middleware para leer JSON
